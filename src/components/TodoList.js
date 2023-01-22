e1ff64a559f4bdc9ca9e153eb3eb45a27575c005
Ryotaro <ryotaro.santa31@gmail.com>
2023-01-22 00:51:16 -0700

    day2
    made some additional features with deleting and checking functions.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,12 @@ import React from 'react';
 //Import Components
 import Todo from './Todo'
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos }) => {
     return(
         <div className='todo-container'>
             <ul className='todo-list'>
                 {todos.map(todo => (
-                    <Todo text={todo.text}/>
+                    <Todo todo={todo} setTodos={setTodos} todos={todos} key={todo.id} text={todo.text}/>
                 ))}
             </ul>
         </div>
