a839b9fcba6dc26d7042d635620cb84b6f74248e
Ryotaro <ryotaro.santa31@gmail.com>
2023-01-25 23:20:09 -0700

    Final Product
    Final product.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Form = ({ setInputText, todos, setTodos, inputText }) => {
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
     const inputTextHandler = (e) => {
         setInputText(e.target.value);
     };
@@ -11,15 +11,18 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
             {text: inputText, completed: false, id: Math.random() * 1000},
         ]);
         setInputText("");
+    };
+    const statusHandler = (e) => {
+        setStatus(e.target.value);
     }
     return(
         <form>
             <input value={inputText} onChange={inputTextHandler} type="text" className='todo-input' />
-            <button className='todo-button' type='submit'>
-                <i onClick={submitTodoHandler} className='fas fa-plus-square'></i>
+            <button onClick={submitTodoHandler} className='todo-button' type='submit'>
+                <i className='fas fa-plus-square'></i>
             </button>
             <div className='select'>
-                <select name='todos' className='filter-todo'>
+                <select onChange={statusHandler} name='todos' className='filter-todo'>
                     <option value="all">All</option>
                     <option value="completed">Completed</option>
                     <option value="uncompleted">Uncompleted</option>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -17,9 +17,11 @@ const Todo = ({text, todo, todos, setTodos}) => {
         }))
     }
 
+    if(todo.text === '') return;
+
     return(
         <div className='todo'>
-            <li className={`todo-item ${todo.completed ? "completed" : ''}`}>{text}</li>
+            <li className={`todo-item ${todo.completed ? "completed" : ''}`}>{todo.text}</li>
             <button onClick={completeHandler} className='complete-btn'>
                 <i className='fas fa-check'></i>
                 </button>
